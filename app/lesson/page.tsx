52487d5683aa914b14028718fa8ca49f98090649
Rahul_Singh <koderrahul@gmail.com>
2024-08-20 11:30:14 +0530

    addtion of quiz

--- app/lesson/page.tsx
+++ app/lesson/page.tsx
@@ -1,6 +1,7 @@
 import { getLesson, getUserProgress } from "@/db/queries";
 import { redirect } from "next/navigation";
 import React from "react";
+import { Quiz } from "./quiz";
 
 const LessonPage = async () => {
   const lessonData = getLesson();
@@ -15,7 +16,20 @@ const LessonPage = async () => {
     redirect("/learn");
   }
 
-  return <div>LessonPage</div>;
+  const initialPercentage =
+    (lesson.challenges.filter((challenge) => challenge.completed).length /
+      lesson.challenges.length) *
+    100;
+
+  return (
+    <Quiz
+      initialLessonId={lesson.id}
+      initialLessonChallenges={lesson.challenges}
+      initialHearts={userProgress.hearts}
+      initialPercentage={initialPercentage}
+      userSubscription={null}
+    />
+  );
 };
 
 export default LessonPage;
